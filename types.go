1de981ce5cd87377cd6bbdd6aff6b982e0edc55c
aliebrahim <alikebrahim@me.com>
2023-12-31 18:46:34 +0300

    Added structs for Google Map APIs

--- types.go
+++ types.go
@@ -20,27 +20,84 @@ type ArtistRender struct {
 	CreationDate   int                 `json:"creationDate"`
 	FirstAlbum     string              `json:"firstAlbum"`
 	DatesLocations map[string][]string `json:"datesLocations"`
+	MapDetails     MapLocations        `json:"map_details"`
 }
 
+type MapLocations struct {
+	MapURL    string        `json:"map_url"`
+	Locations []GeoLocation `json:"locations"`
+}
+
+type GeoLocation struct {
+	Location string  `json:"location"`
+	Lat      float64 `json:"lat"`
+	Lng      float64 `json:"lng"`
+}
+
+// locations API
 type Locations struct {
 	Index []LocationsIndex `json:"index"`
 }
-type Dates struct {
-	Index []DatesIndex `json:"index"`
-}
-type Relations struct {
-	Index []RelationsIndex `json:"index"`
-}
+
 type LocationsIndex struct {
 	ID        int      `json:"id"`
 	Locations []string `json:"locations"`
 	Dates     string   `json:"dates"`
 }
+
+// Relations API
+type RelationsIndex struct {
+	ID             int                 `json:"id"`
+	DatesLocations map[string][]string `json:"datesLocations"`
+}
+
+type Relations struct {
+	Index []RelationsIndex `json:"index"`
+}
+
+// I forgot API
 type DatesIndex struct {
 	ID    int      `json:"id"`
 	Dates []string `json:"dates"`
 }
-type RelationsIndex struct {
-	ID             int                 `json:"id"`
-	DatesLocations map[string][]string `json:"datesLocations"`
+
+type Dates struct {
+	Index []DatesIndex `json:"index"`
+}
+
+// Geocoding API
+type GeocodingResponse struct {
+	Results []Result `json:"results"`
+	Status  string   `json:"status"`
+}
+
+type Result struct {
+	AddressComponents []AddressComponent `json:"address_components"`
+	FormattedAddress  string             `json:"formatted_address"`
+	Geometry          Geometry           `json:"geometry"`
+	PlaceID           string             `json:"place_id"`
+	Types             []string           `json:"types"`
+}
+
+type AddressComponent struct {
+	LongName  string   `json:"long_name"`
+	ShortName string   `json:"short_name"`
+	Types     []string `json:"types"`
+}
+
+type Geometry struct {
+	Bounds       Bounds     `json:"bounds"`
+	Location     Coordinate `json:"location"`
+	LocationType string     `json:"location_type"`
+	Viewport     Bounds     `json:"viewport"`
+}
+
+type Bounds struct {
+	Northeast Coordinate `json:"northeast"`
+	Southwest Coordinate `json:"southwest"`
+}
+
+type Coordinate struct {
+	Lat float64 `json:"lat"`
+	Lng float64 `json:"lng"`
 }
