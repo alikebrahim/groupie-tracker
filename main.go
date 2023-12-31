f4ae0086d56c6b7557811f228513307ca80d83d4
aliebrahim <alikebrahim@me.com>
2023-12-31 18:48:10 +0300

    Geocoding and map generation with Google Maps APIs implemented

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"io"
 	"net/http"
+	"net/url"
 	"strings"
 	"sync"
 	"time"
@@ -33,25 +36,15 @@ func indexRouter(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.Method)
 	switch r.Method {
 	case "GET":
-		artistHanlder(w, r)
+		indexHandler(w, r)
 	case "POST":
 		filterHandler(w, r)
 	}
 }
 
 func filterHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO: rewrite the code to check for which filters have been set and then filter gradually (e.g using creation date first, then filter the list into another with number members)
-	// TODO: fix bug where previous filter results are added to new filter when reloading the website
 	var filteredArtists []ArtistRender
 	creationYearFrom, creationYearTo, firstAlbumYearFrom, firstAlbumYearTo, location, membersNum := FormParse(r)
-	fmt.Println("***** Start - filterHandler: FormParse ******")
-	fmt.Println("creation date from: ", creationYearFrom)
-	fmt.Println("creation date to: ", creationYearTo)
-	fmt.Println("first album from: ", firstAlbumYearFrom)
-	fmt.Println("first album to: ", firstAlbumYearTo)
-	fmt.Println("location: ", location)
-	fmt.Println("members: ", membersNum)
-	fmt.Println("***** End - filterHandler: FormParse ******")
 
 	// filter loop
 	if creationYearFrom >= 1958 {
@@ -176,31 +169,12 @@ func filterHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	// Other filters
-
-	// for id, artist := range Artists {
-	// 	location = strings.Split(location, ",")[0]
-	// 	artistFirstAlbumYear, loc := GetInfo(id, strings.ToLower(location), artist)
-	// 	if (artist.CreationDate >= creationYearFrom && artist.CreationDate <= creationYearTo) || (artistFirstAlbumYear >= firstAlbumYearFrom && artistFirstAlbumYear <= firstAlbumYearTo) || loc || len(artist.Members) == membersNum {
-	// 		// fmt.Printf("Artist:%v Creation Date:%v, First Album:%v \n", artist.Name, artist.CreationDate, artist.FirstAlbum)
-	// 		artistMatch := ArtistRender{
-	// 			ID:             Artists[id].ID,
-	// 			Image:          Artists[id].Image,
-	// 			Name:           Artists[id].Name,
-	// 			Members:        Artists[id].Members,
-	// 			CreationDate:   Artists[id].CreationDate,
-	// 			FirstAlbum:     Artists[id].FirstAlbum,
-	// 			DatesLocations: relations.Index[id].DatesLocations,
-	// 		}
-	// 		filteredArtists = append(filteredArtists, artistMatch)
-	// 	}
-	// }
 	tmpl := template.Must(template.ParseFiles("assets/templates/filter.html"))
 	if err := tmpl.Execute(w, filteredArtists); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
-func artistHanlder(w http.ResponseWriter, r *http.Request) {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
 	wg := &sync.WaitGroup{}
 
@@ -227,7 +201,6 @@ func artistHanlder(w http.ResponseWriter, r *http.Request) {
 
 func artistHandler(w http.ResponseWriter, r *http.Request) {
 	id := GetID(r.URL.Query().Get("id")) - 1
-	// fmt.Println(id)
 	artist := ArtistRender{
 		ID:             Artists[id].ID,
 		Image:          Artists[id].Image,
@@ -238,12 +211,99 @@ func artistHandler(w http.ResponseWriter, r *http.Request) {
 		DatesLocations: relations.Index[id].DatesLocations,
 	}
 
+	artist.MapDetails.Locations = Geocoding(artist)
+
+	artist.MapDetails.MapURL = CreatMap(artist)
+	// fmt.Println("From artistHanderl (fullUrl)\n", artist.MapDetails.mapURL)
+	// for _, loc := range artist.MapDetails.Locations {
+	// 	fmt.Println(loc.Location + ":")
+	// 	fmt.Printf("Lat: (%v) Lng: (%v)\n", loc.Lat, loc.Lng)
+	// }
+
+	// Geo := GeoLocation{}
+	// _, Geo.Lat, Geo.Lng = Geocoding(artist)
+	// artist.Location = append(artist.Location, Geo)
+	// fmt.Printf("Latitude: (%f) Longtitude: (%f)\n", artist.Location[0].Lat, artist.Location[0].Lng)
+
 	tmpl := template.Must(template.New("artist.html").Funcs(template.FuncMap{
 		"FormatText": FormatText,
 	}).ParseFiles("assets/templates/artist.html"))
-	// tmpl := template.Must(template.ParseFiles("assets/templates/artist.html"))
 	if err := tmpl.Execute(w, artist); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
+}
+
+func Geocoding(artist ArtistRender) []GeoLocation {
+	var (
+		GeoLocations []GeoLocation
+		APIKey       = "AIzaSyD0AeIdWqfSMZujmXzaOHAQx1deLoYFnFE"
+		baseURL      = "https://maps.googleapis.com/maps/api/geocode/json?"
+	)
+
+	for loc := range artist.DatesLocations {
+		var (
+			respData        GeocodingResponse
+			locationAddress GeoLocation
+			address         = strings.ReplaceAll(loc, "-", ",")
+		)
+
+		params := url.Values{}
+		params.Add("address", address)
+		params.Add("key", APIKey)
+
+		resp, err := http.Get(baseURL + params.Encode())
+		if err != nil {
+			fmt.Println("Error making request")
+			return nil
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Error reading response", err)
+			return nil
+		}
+
+		err = json.Unmarshal(body, &respData)
+		if err != nil {
+			fmt.Println("failed to unmarshall data", err)
+		}
+
+		// fmt.Println("Address", address)
+		// fmt.Println("Formatted Address", respData.Results[0].FormattedAddress)
+		// fmt.Printf("Lat: (%v) Lng: (%v)\n", respData.Results[0].Geometry.Location.Lat, respData.Results[0].Geometry.Location.Lng)
+
+		locationAddress.Location = respData.Results[0].FormattedAddress
+		locationAddress.Lat, locationAddress.Lng = respData.Results[0].Geometry.Location.Lat, respData.Results[0].Geometry.Location.Lng
+
+		GeoLocations = append(GeoLocations, locationAddress)
+	}
+
+	return GeoLocations
+}
+
+func CreatMap(artist ArtistRender) string {
+	var (
+		baseURL string = "https://maps.googleapis.com/maps/api/staticmap?"
+		APIKey  string = "AIzaSyD0AeIdWqfSMZujmXzaOHAQx1deLoYFnFE"
+
+		// mapCenter string = "41.0082,28.9784"
+		// zoom    string = "2"
+		size    string = "500x400"
+		markers string
+	)
+	fmt.Println("Markers:\n", markers)
+
+	params := url.Values{}
+	// params.Add("center", mapCenter)
+	params.Add("size", size)
+	// params.Add("zoom", zoom)
+	for _, loc := range artist.MapDetails.Locations {
+		marker := fmt.Sprintf("%v,%v", loc.Lat, loc.Lng)
+		params.Add("markers", marker)
+	}
+	params.Add("key", APIKey)
+
+	return baseURL + params.Encode()
 }
